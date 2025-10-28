a795f7f46c0c319edcfe7b73aed10ff285040b31
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2025-10-28 11:51:49 +0100

    Make the "loaded" boolean truthful

--- src/embeddedState.ts
+++ src/embeddedState.ts
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const dataElmnt = document.getElementById('data');
     if(dataElmnt) {
         state.state = JSON.parse(atob((dataElmnt.innerHTML).split('').reverse().join('')));
+        state.loaded = true;
     }
 })
 
